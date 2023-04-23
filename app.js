dfb1083785ff7932bc081c8d713d4d143c732e12
OlenaTymchenkoLeo <124084136+OlenaTymchenkoLeo@users.noreply.github.com>
2023-04-23 15:46:07 +0300

    feat: add pressing a key functionality

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const body = document.querySelector('body');
+let h1 = document.createElement('h1');
+body.insertAdjacentElement('afterbegin', h1);
+h1.textContent = 'RSS Virtual Keyboard';
 let textarea = document.createElement('textarea');
 textarea.style.display = 'block';
-body.insertAdjacentElement('afterbegin', textarea);
+body.insertAdjacentElement('beforeend', textarea);
 let main = document.createElement('main');
 main.classList.add("keyboard");
-body.insertAdjacentElement('afterbegin', main);
+body.insertAdjacentElement('beforeend', main);
+let p1 = document.createElement('p');
+body.insertAdjacentElement('beforeend', p1);
+p1.textContent = 'Клавиатура создана в операционной системе Windows';
+let p2 = document.createElement('p');
+body.insertAdjacentElement('beforeend', p2);
+p2.textContent = 'Для переключения языка раскладки: Shift + Ctrl';
 
 
 let row1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
-let row2 = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}',  'Del'];
-let row3 = ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '|', 'Enter'];
-let row4 = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift', 'up'];
+let row2 = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}',  'Del'];
+let row3 = ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', '|', 'Enter'];
+let row4 = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', 'Shift', 'up'];
 let row5 = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'down', 'right'];
 
 let row1Sh = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
@@ -66,3 +75,34 @@ main.append(createRow(row3));
 main.append(createRow(row4));
 main.append(createRow(row5));
 
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.key);
+  let currentButton = document.querySelectorAll('button'); 
+ currentButton.forEach(elem =>{
+  if (elem.textContent  === e.key) {
+    elem.classList.add("active")
+    textarea.value += e.key;
+  }  
+ })
+});
+document.addEventListener('keyup', function(e) {
+  //console.log(e.key);
+  let currentButton = document.querySelectorAll('button'); 
+ currentButton.forEach(elem =>{
+  if (elem.textContent  === e.key) {
+    elem.classList.remove("active")
+  }  
+ })
+});
+  textarea.value = '';
+  let buttons = document.querySelectorAll('button');
+document.addEventListener('click', function(e) {
+  console.log(e.target);
+buttons.forEach(elem => {   
+  if(e.target === elem) {
+    let current = e.target;
+    textarea.value += current.textContent;
+  }  
+})  
+});
