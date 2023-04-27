6f161d7e5ad50a289046ab7c8c76588619c8d5bb
OlenaTymchenkoLeo <124084136+OlenaTymchenkoLeo@users.noreply.github.com>
2023-04-27 22:53:27 +0300

    feat: add animation

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
+/* eslint-disable no-plusplus */
 const body = document.querySelector('body');
-let h1 = document.createElement('h1');
+const h1 = document.createElement('h1');
 body.insertAdjacentElement('afterbegin', h1);
 h1.textContent = 'RSS Virtual Keyboard';
-let textarea = document.createElement('textarea');
+const textarea = document.createElement('textarea');
 textarea.style.display = 'block';
+textarea.setAttribute('autofocus', 'autofocus');
 body.insertAdjacentElement('beforeend', textarea);
-let main = document.createElement('main');
-main.classList.add("keyboard");
+const main = document.createElement('main');
+main.classList.add('keyboard');
 body.insertAdjacentElement('beforeend', main);
-let p1 = document.createElement('p');
+const p1 = document.createElement('p');
 body.insertAdjacentElement('beforeend', p1);
 p1.textContent = 'Клавиатура создана в операционной системе Windows';
-let p2 = document.createElement('p');
+const p2 = document.createElement('p');
 body.insertAdjacentElement('beforeend', p2);
 p2.textContent = 'Для переключения языка раскладки: Shift + Ctrl';
 
+const row1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
+const row2 = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', 'Del'];
+const row3 = ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', '|', 'Enter'];
+const row4 = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', 'Shift', 'up'];
+const row5 = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'down', 'right'];
 
-let row1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
-let row2 = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}',  'Del'];
-let row3 = ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', '|', 'Enter'];
-let row4 = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', 'Shift', 'up'];
-let row5 = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'down', 'right'];
+const row1Sh = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
+const row2Sh = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 'Del'];
+const row3Sh = ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '|', 'Enter'];
+const row4Sh = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift', 'up'];
+const row5Sh = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'down', 'right'];
 
-let row1Sh = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
+const arr1Row = [row1, row2, row3, row4, row5];
+const arr2Row = [row1Sh, row2Sh, row3Sh, row4Sh, row5Sh];
 
-//Create one button
+// Create one button
 function createButton(text) {
-  let button = document.createElement('button');
-    button.classList.add('key');
-  button.textContent = text;  
-  if (button.textContent === 'Space') {button.classList.add('space');}
-  if (button.textContent === 'Tab' || button.textContent === 'Caps Lock' || button.textContent === 'Shift' ||
-  button.textContent === 'Ctrl' || button.textContent === 'Win' || button.textContent === 'Alt' ||
-  button.textContent === 'Space' || button.textContent === 'Backspace' || button.textContent === 'Del' ||
-  button.textContent === 'Enter') {button.classList.add('utility');}
+  const button = document.createElement('button');
+  button.classList.add('key');
+  button.textContent = text;
+  if (button.textContent === 'Space') { button.classList.add('space'); }
+  if (button.textContent === 'Tab' || button.textContent === 'Caps Lock' || button.textContent === 'Shift'
+  || button.textContent === 'Ctrl' || button.textContent === 'Win' || button.textContent === 'Alt'
+  || button.textContent === 'Space' || button.textContent === 'Backspace' || button.textContent === 'Del'
+  || button.textContent === 'Enter') { button.classList.add('utility'); }
   if (button.textContent === 'up') {
     button.textContent = '';
     button.innerHTML = '<img src= "img/Arrow-up.png">';
-    button.classList.add('arrows');      
+    button.classList.add('arrows');
   }
   if (button.textContent === 'left') {
     button.textContent = '';
@@ -57,52 +65,121 @@ function createButton(text) {
   return button;
 }
 
-//Create row of buttons
-function createRow(row) {
-  let section = document.createElement("section");
-  section.classList.add("row");  
-for (let i = 0; i < row.length; i++) {    
-   let currentButton = createButton(row[i]);   
-   section.append(currentButton); 
+// Create row of buttons
+function createRow(row, classSec) {
+  const section = document.createElement('section');
+  section.classList.add(classSec);
+  for (let i = 0; i < row.length; i++) {
+    const currentButton = createButton(row[i]);
+    section.append(currentButton);
+  }
+  return section;
 }
-return section;
+
+// Create all keybords
+function createAllButtons(arr, classSec) {
+  main.append(createRow(arr[0], classSec));
+  main.append(createRow(arr[1], classSec));
+  main.append(createRow(arr[2], classSec));
+  main.append(createRow(arr[3], classSec));
+  main.append(createRow(arr[4], classSec));
+}
+createAllButtons(arr1Row, 'row');
+
+// Functionality of textarea
+// Press down key
+function printKeydownOnKeyboard(e) {
+  const currentButton = document.querySelectorAll('button');
+  currentButton.forEach((elem) => {
+    if (elem.textContent === e.key) {
+      elem.classList.add('active');
+      if (!elem.classList.contains('utility')) {
+        textarea.value += e.key;
+      }
+      if (elem.textContent === 'Shift' && elem.textContent === 'Ctrl') {
+        console.log('hey');
+      }
+    }
+  });
+}
+document.addEventListener('keydown', printKeydownOnKeyboard);
+
+// Press Up key
+function printKeyupOnKeyboard(e) {
+  const currentButton = document.querySelectorAll('button');
+  currentButton.forEach((elem) => {
+    if (elem.textContent === e.key) {
+      elem.classList.remove('active');
+    }
+  });
+}
+document.addEventListener('keyup', printKeyupOnKeyboard);
+
+// Click mouse on key
+// textarea.value = '';
+// let caps = false;
+const rowSection = document.querySelectorAll('.row');
+const rowShSection = document.querySelectorAll('.rowSh');
+const buttons = document.querySelectorAll('button');
+function printOnClick(e) {
+  buttons.forEach((elem) => {
+    if (e.target === elem) {
+      const current = e.target;
+      switch (current.innerHTML) {
+        case 'Backspace':
+          textarea.value.substring(0, textarea.value.length - 1);
+          break;
+        case 'Enter':
+          textarea.value += '\n';
+          break;
+        case 'Space':
+          textarea.value += ' ';
+          break;
+        case 'Alt':
+          textarea.value += '';
+          break;
+        case 'Win':
+          textarea.value += '';
+          break;
+        case 'Ctrl':
+          textarea.value += '';
+          break;
+        default:
+          textarea.value += current.innerHTML;
+      }
+    }
+  });
+}
+/*
+    if (e.target === elem && !elem.classList.contains('utility')) {
+      const current = e.target;
+      textarea.value += current.textContent;
+    }
+*/
+document.addEventListener('click', printOnClick);
+
+// Functionality for Shift
+function shiftDownHandler(e) {
+  // const rowSection = document.querySelectorAll('.row');
+  if (e.code === 'ShiftLeft') {
+    rowSection.forEach((elem) => elem.remove());
+    createAllButtons(arr2Row, 'rowSh');
+  }
+  e.repeat = false;
+  document.removeEventListener('keydown', shiftDownHandler);
+  document.addEventListener('keydown', shiftDownHandler);
 }
 
-//Create all keybords
-main.append(createRow(row1));  
-main.append(createRow(row2));
-main.append(createRow(row3));  
-main.append(createRow(row4));
-main.append(createRow(row5));
+function shiftUpHandler(e) {
+  // const rowShSection = document.querySelectorAll('.rowSh');
+  if (e.code === 'ShiftLeft') {
+    rowShSection.forEach((elem) => elem.remove());
+    createAllButtons(arr1Row, 'row');
+  }
+  document.removeEventListener('keyup', shiftUpHandler);
+  document.addEventListener('keyup', shiftUpHandler);
+}
 
-
-document.addEventListener('keydown', function(e) {
-  console.log(e.key);
-  let currentButton = document.querySelectorAll('button'); 
- currentButton.forEach(elem =>{
-  if (elem.textContent  === e.key) {
-    elem.classList.add("active")
-    textarea.value += e.key;
-  }  
- })
-});
-document.addEventListener('keyup', function(e) {
-  //console.log(e.key);
-  let currentButton = document.querySelectorAll('button'); 
- currentButton.forEach(elem =>{
-  if (elem.textContent  === e.key) {
-    elem.classList.remove("active")
-  }  
- })
-});
-  textarea.value = '';
-  let buttons = document.querySelectorAll('button');
-document.addEventListener('click', function(e) {
-  console.log(e.target);
-buttons.forEach(elem => {   
-  if(e.target === elem) {
-    let current = e.target;
-    textarea.value += current.textContent;
-  }  
-})  
-});
+document.addEventListener('keydown', shiftDownHandler);
+document.addEventListener('keyup', shiftUpHandler);
+document.addEventListener('click', shiftDownHandler);
