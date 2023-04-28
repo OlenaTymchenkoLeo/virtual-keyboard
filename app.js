64310cf7958d531301fcbfc2ca7c714f207b7781
OlenaTymchenkoLeo <124084136+OlenaTymchenkoLeo@users.noreply.github.com>
2023-04-28 14:51:09 +0300

    feat: add change language

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ body.insertAdjacentElement('beforeend', p1);
 p1.textContent = 'Клавиатура создана в операционной системе Windows';
 const p2 = document.createElement('p');
 body.insertAdjacentElement('beforeend', p2);
-p2.textContent = 'Для переключения языка раскладки: Shift + Ctrl';
+p2.textContent = 'Для переключения языка раскладки: Ctrl + Alt';
 
 const row1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
 const row2 = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', 'Del'];
@@ -31,6 +31,22 @@ const row5Sh = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'down', 'r
 
 const arr1Row = [row1, row2, row3, row4, row5];
 const arr2Row = [row1Sh, row2Sh, row3Sh, row4Sh, row5Sh];
+
+const row1u = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
+const row2u = ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ї', 'Del'];
+const row3u = ['Caps Lock', 'ф', 'і', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'є', '|', 'Enter'];
+const row4u = ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '?', 'Shift', 'up'];
+const row5u = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'down', 'right'];
+
+const row1uSh = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
+const row2uSh = ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'х', 'Ї}', 'Del'];
+const row3uSh = ['Caps Lock', 'Ф', 'І', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Є', '|', 'Enter'];
+const row4uSh = ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '?', 'Shift', 'up'];
+const row5uSh = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'left', 'down', 'right'];
+
+const arr1uRow = [row1u, row2u, row3u, row4u, row5u];
+const arr2uRow = [row1uSh, row2uSh, row3uSh, row4uSh, row5uSh];
+let leng = 'en';
 
 // Create one button
 function createButton(text) {
@@ -94,7 +110,7 @@ function printKeydownOnKeyboard(e) {
     if (elem.textContent === e.key) {
       elem.classList.add('active');
       if (!elem.classList.contains('utility')) {
-        textarea.value += e.key;
+        textarea.innerHTML += elem.textContent;
       }
     }
   });
@@ -115,40 +131,63 @@ document.addEventListener('keyup', printKeyupOnKeyboard);
 // Click mouse on key
 
 const buttons = document.querySelectorAll('button');
+
 function printOnClick(e) {
   buttons.forEach((elem) => {
     if (e.target === elem) {
       const current = e.target;
       switch (current.innerHTML) {
         case 'Backspace':
-          textarea.value.substring(0, textarea.value.length - 1);
+          textarea.innerHTML = textarea.innerHTML.slice(0, -1);
+          break;
+        case 'Del':
+          textarea.innerHTML = textarea.innerHTML.slice(0, 1);
           break;
         case 'Enter':
-          textarea.value += '\n';
+          textarea.innerHTML += '\n';
           break;
         case 'Space':
-          textarea.value += ' ';
+          textarea.innerHTML += ' ';
           break;
         case 'Tab':
-          textarea.value += '    ';
+          textarea.innerHTML += '    ';
           break;
         case 'Alt':
-          textarea.value += '';
+          textarea.innerHTML += '';
           break;
         case 'Win':
-          textarea.value += '';
+          textarea.innerHTML += '';
           break;
         case 'Ctrl':
-          textarea.value += '';
+          textarea.innerHTML += '';
+          break;
+        case 'Caps Lock':
+          textarea.innerHTML += '';
           break;
         default:
-          textarea.value += current.innerHTML;
+          textarea.innerHTML += current.innerHTML;
       }
     }
   });
 }
 
 document.addEventListener('click', printOnClick);
+
+// caps lock on click
+let isCaps = false;
+document.addEventListener('click', (e) => {
+  if (e.target.innerHTML === 'Caps Lock' && isCaps === false) {
+    isCaps = true;
+    document.querySelectorAll('.row').forEach((button) => button.remove());
+    createAllButtons(arr2Row, 'rowSh');
+    document.addEventListener('click', printOnClick);
+  } else if (e.target.innerHTML === 'Caps Lock' && isCaps === true) {
+    isCaps = false;
+    document.querySelectorAll('.rowSh').forEach((button) => button.remove());
+    createAllButtons(arr1Row, 'row');
+    document.addEventListener('click', printOnClick);
+  }
+});
 
 // Functionality for Shift
 function shiftDownHandler(e) {
@@ -157,7 +196,11 @@ function shiftDownHandler(e) {
   if (shift.innerHTML === 'Shift') { shift.classList.add('active'); }
   if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
     rowSection.forEach((elem) => elem.remove());
-    createAllButtons(arr2Row, 'rowSh');
+    if (leng === 'en') {
+      createAllButtons(arr2Row, 'rowSh');
+    } else if (leng === 'ukr') {
+      createAllButtons(arr2uRow, 'rowSh');
+    }
     document.removeEventListener('keydown', printKeydownOnKeyboard);
     document.addEventListener('keydown', printKeydownOnKeyboard);
   }
@@ -170,7 +213,11 @@ function shiftUpHandler(e) {
   const rowShSection = document.querySelectorAll('.rowSh');
   if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
     rowShSection.forEach((elem) => elem.remove());
-    createAllButtons(arr1Row, 'row');
+    if (leng === 'en') {
+      createAllButtons(arr1Row, 'row');
+    } else if (leng === 'ukr') {
+      createAllButtons(arr1uRow, 'row');
+    }
   }
   document.removeEventListener('keyup', shiftUpHandler);
   document.addEventListener('keyup', shiftUpHandler);
@@ -179,8 +226,10 @@ function shiftUpHandler(e) {
 document.addEventListener('keydown', shiftDownHandler);
 document.addEventListener('keyup', shiftUpHandler);
 
+// Caps Lock on keydown and change lenguage
+
 document.addEventListener('keydown', (e) => {
-  e.preventDefault();
+// e.preventDefault();
   const isCapsL = e.getModifierState('CapsLock');
   if (e.code === 'CapsLock') {
     if (isCapsL === true) {
@@ -191,5 +240,18 @@ document.addEventListener('keydown', (e) => {
       document.querySelectorAll('.rowSh').forEach((elem) => elem.remove());
       createAllButtons(arr1Row, 'row');
     }
+  }
+  if (e.ctrlKey && e.altKey && leng === 'en') {
+    document.querySelectorAll('.row').forEach((elem) => elem.remove());
+    createAllButtons(arr1uRow, 'row');
+    leng = 'ukr';
+    document.removeEventListener('keydown', printKeydownOnKeyboard);
+    document.addEventListener('keydown', printKeydownOnKeyboard);
+  } else if (e.ctrlKey && e.altKey && leng === 'ukr') {
+    document.querySelectorAll('.row').forEach((elem) => elem.remove());
+    createAllButtons(arr1Row, 'row');
+    leng = 'en';
+    document.removeEventListener('keydown', printKeydownOnKeyboard);
+    document.addEventListener('keydown', printKeydownOnKeyboard);
   }
 });
